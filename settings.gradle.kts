ae54fad82839eba0ae677e783c33148d04224ce2
jeffreycarver <jxc3149@mavs.uta.edu>
2023-10-23 14:53:02 -0500

    Initial push.

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "3310_Group8_GSGD"
+include(":app")
